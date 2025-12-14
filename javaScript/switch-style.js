0eec5f1b8b9c0f3640f439c27b316be2b32f8db7
kawthar <kawthar.CHAIB-EDDOUR-ITIM@etu.univ-amu.fr>
2025-12-14 18:23:22 +0100

    j'ai changer le link du css dans le html pour qu'il puisse être en accorde avec le javascript et normalement on peut changer le style su site parfaitement
    :wq
    
    				:
    wq

--- javaScript/switch-style.js
+++ javaScript/switch-style.js
@@ -1,5 +1,22 @@
-switchstyle.js
-/* Script to switch between stylesheets */
-alert("JavaScript connecté !");
-alert("JS OK !");
-console.log("JavaScript connecté !)");
+// Script to switch between stylesheets
+document.addEventListener("DOMContentLoaded", () => {
+    const themeButton = document.getElementById("theme-toggle");
+    const themeLink = document.getElementById("theme-style");
+
+    // Charger le thème sauvegardé au démarrage de la page
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+        themeLink.setAttribute("href", savedTheme);
+    }
+
+    // Changer de thème au clic
+    themeButton.addEventListener("click", () => {
+        if(themeLink.getAttribute("href") === "css/style.css") {
+            themeLink.setAttribute("href", "css/style.diff.css");
+            localStorage.setItem("theme", "css/style.diff.css"); // Sauvegarder le choix
+        } else {
+            themeLink.setAttribute("href", "css/style.css");
+            localStorage.setItem("theme", "css/style.css"); // Sauvegarder le choix
+        }
+    });
+});
