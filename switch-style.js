4b8008fe14d39ca10227670be67c6ba16b2752e8
Maelan <maelan.peyron@etu.univ-amu.fr>
2025-12-17 05:50:31 +0100

    ajout du css mode claire et de la lune pour changer de mode

--- switch-style.js
+++ switch-style.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const themeButton = document.getElementById("theme-toggle");
     const themeLink = document.getElementById("theme-style");
-
+    const luneIcon = document.getElementById("luneToggle");
     // Charger le thème sauvegardé au démarrage de la page
     const savedTheme = localStorage.getItem("theme");
     if (savedTheme) {
@@ -11,13 +11,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Changer de thème au clic
     themeButton.addEventListener("click", () => {
-        if(themeLink.getAttribute("href") === "css/style.dark.css") {
+        if(themeLink.getAttribute("href") === "css/style.dark.css" || themeLink.getAttribute("href") === "css/style.bright.css") {
             themeLink.setAttribute("href", "css/style.diff.css");
             localStorage.setItem("theme", "css/style.diff.css"); // Sauvegarder le choix
         } else {
             themeLink.setAttribute("href", "css/style.dark.css");
             localStorage.setItem("theme", "css/style.dark.css"); // Sauvegarder le choix
         }
+        });
+    luneIcon.addEventListener("click", () => {   
+        if (themeLink.getAttribute("href") === "css/style.dark.css") {
+            themeLink.setAttribute("href", "css/style.bright.css");
+            localStorage.setItem("theme", "css/style.bright.css");
+        } else {
+            themeLink.setAttribute("href", "css/style.dark.css");
+            localStorage.setItem("theme", "css/style.dark.css");
+        }
+        
     });
 });
 
